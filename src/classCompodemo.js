48e0d240f56a60cb814b83de1302dc743ac0a17a
bhagitha <bhagithaparamesh@gmail.com>
2022-01-07 17:31:57 +0530

    Initial commit

---
+++ src/classCompodemo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class classCompodemo extends Component {
+constructor(
+    state={username:"", password:""}
+)
+    render() {
+        return (
+            <div>
+                
+           value={this.state.username}
+
+            </div>
+        )
+    }
+}
